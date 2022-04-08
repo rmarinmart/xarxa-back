d4da8165be535ffb93f266c7525ed1b9c294ee64
rmmcoder <45757618+rmmcoder@users.noreply.github.com>
2022-04-08 10:34:35 +0200

    cors habilitado por defecto

--- xarxa-server/server.js
+++ xarxa-server/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-var corsOptions = {
+/*var corsOptions = {
   //origin: "http://localhost:3000",
   origin: "*",
-};
-app.use(cors(corsOptions));
+};*/
+app.use(cors());
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
